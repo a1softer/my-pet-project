5adde417c7674ce5732336170bef705644681677
a1softer <andrew.andrianov2016@yandex.ru>
2025-10-12 14:19:39 +0700

    feat: добавлена реализация умных перечислений

--- src/ConstructionEquipmentRentalService/domain/Domain/Клиент/Статус бронирования.cs
+++ src/ConstructionEquipmentRentalService/domain/Domain/Клиент/Статус бронирования.cs
@@ -1,9 +1,54 @@
-﻿namespace Domain.Клиент
+﻿using Domain.Shared;
+
+namespace Domain.Клиент
 {
-    public enum Статус_бронирования
+    public abstract class СтатусБронирования : Enumeration<СтатусБронирования>
     {
-        В_ожидании = 1,
-        Подтверждено = 2,
-        Отменено = 3
+        protected СтатусБронирования(int key, string name)
+            : base(key, name) { }
+
+        public abstract bool МожноОтменить();
+        public abstract bool МожноПодтвердить();
+        public abstract string ПолучитьЦвет();
+    }
+
+    public sealed class СтатусБронированияВОжидании : СтатусБронирования
+    {
+        public СтатусБронированияВОжидании()
+            : base(1, "В ожидании") { }
+
+        public override bool МожноОтменить() => true;
+        public override bool МожноПодтвердить() => true;
+        public override string ПолучитьЦвет() => "желтый";
+    }
+
+    public sealed class СтатусБронированияПодтверждено : СтатусБронирования
+    {
+        public СтатусБронированияПодтверждено()
+            : base(2, "Подтверждено") { }
+
+        public override bool МожноОтменить() => true;
+        public override bool МожноПодтвердить() => false;
+        public override string ПолучитьЦвет() => "зеленый";
+    }
+
+    public sealed class СтатусБронированияОтменено : СтатусБронирования
+    {
+        public СтатусБронированияОтменено()
+            : base(3, "Отменено") { }
+
+        public override bool МожноОтменить() => false;
+        public override bool МожноПодтвердить() => false;
+        public override string ПолучитьЦвет() => "красный";
+    }
+
+    public sealed class СтатусБронированияЗавершено : СтатусБронирования
+    {
+        public СтатусБронированияЗавершено()
+            : base(4, "Завершено") { }
+
+        public override bool МожноОтменить() => false;
+        public override bool МожноПодтвердить() => false;
+        public override string ПолучитьЦвет() => "синий";
     }
 }
--- src/ConstructionEquipmentRentalService/domain/Domain/Оборудование/Оборудование.cs
+++ src/ConstructionEquipmentRentalService/domain/Domain/Оборудование/Оборудование.cs
@@ -2,7 +2,7 @@
 {
     public class Equipment
     {
-        public Equipment(IDEquipment id, RentalCost rentalCostPerHour, EquipmentModel model, EquipmentType type, LastDateTO lastMaintenanceDate, StateProcent wearPrecentage)
+        public Equipment(IDEquipment id, RentalCost rentalCostPerHour, EquipmentModel model, ТипОборудования type, LastDateTO lastMaintenanceDate, StateProcent wearPrecentage)
         {
             Id = id;
             RentalCostPerHour = rentalCostPerHour;
@@ -14,7 +14,7 @@
         public IDEquipment Id { get; }
         public RentalCost RentalCostPerHour { get; private set; }
         public EquipmentModel Model { get; private set; }
-        public EquipmentType Type { get; private set; }
+        public ТипОборудования Type { get; private set; }
         public LastDateTO LastMaintenanceDate { get; private set; }
         public StateProcent WearPrecentage { get; private set; }
     }

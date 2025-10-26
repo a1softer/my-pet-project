87546ce2819c5deef569ec17b95155e50d8ff7a6
a1softer <andrew.andrianov2016@yandex.ru>
2025-10-27 00:28:02 +0700

    feat: реализовано чтение конфигурации ASP.NET Core

---
+++ src/ConstructionEquipmentRentalService/api/ConstructionEquipmentRentalService/ConstructionEquipmentRentalService/TestConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ConstructionEquipmentRentalService
+{
+    public class TestConfiguration
+    {
+        public string ApplicationName { get; set; } = string.Empty;
+        public string Environment { get; set; } = string.Empty;
+        public int MaxRetryCount { get; set; }
+    }
+}

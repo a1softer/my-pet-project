24621022c6e055dc18355285d19ee40bb214f9d5
a1softer <andrew.andrianov2016@yandex.ru>
2026-01-07 17:45:08 +0700

    connecting postgresql

---
+++ src/ConstructionEquipmentRentalService/api/ConstructionEquipmentRentalService/ConstructionEquipmentRentals.Infrastructure/InfrastructureInjection.cs
@@ -0,0 +1,15 @@
+﻿using ConstructionEquipmentRentals.Infrastructure.Common;
+using ConstructionEquipmentRentals.Infrastructure.Common.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConstructionEquipmentRentals.Infrastructure
+{
+    public static class InfrastructureInjection
+    {
+        public static void Inject(this IServiceCollection services)
+        {
+            services.AddOptions<PostgresConnectionOptions>().BindConfiguration("PostgresConnectionOptions");
+            services.AddScoped<ApplicationDbContext>();
+        }
+    }
+}
